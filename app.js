edc9ae167e1aa5cb3a76d8ccfca256c582118562
Diego Granados <diego@codeando.edu.pe>
2021-12-11 13:24:42 -0400

    Calcular tu edad en años de perro

---
+++ app.js
@@ -0,0 +1,22 @@
+//Mi edad
+const myAge = 5;
+
+//Edad temprana
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//Edad Adulta
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+
+//Suma de las dos etapas
+let myAgeInDogYears = earlyYears + laterYears;
+
+//Nombre de la Persona
+const myName= "Diego".toLowerCase();
+
+//Mensaje
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
